0359de6b602d73ea364917ad748f7faaef82fdc1
Imad Siddiqui <imadsiddiqui86@gmail.com>
2022-12-25 18:41:45 -0600

    Obtain current user membership id and type

--- destiny_api.py
+++ destiny_api.py
@@ -10,19 +10,18 @@ api_key = os.getenv('API_KEY')
 client_id = os.getenv('CLIENT_ID')
 client_secret = os.getenv('CLIENT_SECRET')
 
-redirect_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ" 
+redirect_url = "https://www.google.com"    
 auth_url = "https://www.bungie.net/en/OAuth/Authorize"
 token_url = "https://www.bungie.net/platform/app/oauth/token/"
-xur_url = "https://www.bungie.net/Platform/Destiny2/Vendors/?components=400,402"
+user_url = "https://www.bungie.net/Platform/User/GetMembershipsForCurrentUser/"
 
 session = OAuth2Session(client_id=client_id, redirect_uri=redirect_url)
 
 auth_link = session.authorization_url(auth_url)
-print(f"Authorization link: {auth_link[0]}")
-
+print(f"{auth_link[0]}")
 redirect_response = input(f"Paste redirect link here: ")
 
-session.fetch_token(
+token = session.fetch_token(
     client_id=client_id,
     client_secret=client_secret,
     token_url=token_url,
@@ -31,13 +30,16 @@ session.fetch_token(
 header = { 
     'X-API-Key': api_key 
 }
-
-response = session.get(url=xur_url, headers=header)
-
+print(token)
+#print(f"Access token: {token['access_token']} \n Refresh token: {token['refresh_token']}")
+response = session.get(url=user_url, headers=header)
 data = json.loads(response.text)
-###item = data['Response']['sales']['data']['2190858386']['saleItems']['0']
-###print(f"{item['itemHash']}")
 
+membership_id = data['Response']['destinyMemberships'][0]['membershipId']
+membership_type = data['Response']['destinyMemberships'][0]['membershipType']
+
+
+"""""
 if(data['Response']['vendors']['data']['2190858386']['enabled']):
     print("Xur is available today")
 else:
@@ -45,16 +47,4 @@ else:
 
 for items in data['Response']['sales']['data']['2190858386']['saleItems']:
     print(f"{data['Response']['sales']['data']['2190858386']['saleItems'][items]['itemHash']}")
-
-
-# Print response
 """""
-print("\n\n\n\n")
-print(f"Response Status: {response.status_code}")
-print(f"Response Reason: {response.reason}")
-print(f"Data: \n{data}")
-
-"""""
-
-
-#Xur location and inventory
